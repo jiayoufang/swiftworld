6e60922d97c80f9676c1d0ebe9b665d991141ae4
fangjiayou <fangjiayou@fangjiayoudeMacBook-Pro.local>
2015-04-02 22:01:24 +0800

    调用其他app，打电话，发短信，打开网页，发邮件

--- SwiftDemo/SwiftDemo/ViewController.swift
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -14,7 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func openApp(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string:"ime://i.love.you")!)
+    }
+    
+    @IBAction func openBaidu(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://www.baidu.com")!)
+    }
+    @IBAction func callTelphone(sender: AnyObject) {
+        
+        UIApplication.sharedApplication().openURL(NSURL(string: "tel://10086")!)
+    }
+    @IBAction func sendEmail(sender: AnyObject) {
+        
+        UIApplication.sharedApplication().openURL(NSURL(string: "mailto://xxx@qq.com")!)
+    }
+    @IBAction func sendSms(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "sms://10086")!)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
